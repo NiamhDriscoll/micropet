79065f5ca4cf8cae2b287368f21358eeab3b8573
Niamh Driscoll <niamhmolson@outlook.com>
2024-12-20 12:36:04 -0800

    Rotated it all

--- main.py
+++ main.py
@@ -1,50 +1,80 @@
+def worm():
+    basic.show_leds("""
+        . # . . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . . # . .
+        """)
+    basic.pause(10)
+    basic.show_leds("""
+        . . # . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . # . . .
+        """)
+    basic.show_leds("""
+        . # . . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . . # . .
+        """)
+    basic.pause(10)
+    basic.show_leds("""
+        . . # . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . # . . .
+        """)
+    basic.show_leds("""
+        . . . . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . . . . .
+        """)
+
 def on_button_pressed_a():
     squalk()
-def on_button_pressed_b():
-    worm()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def squalk():
     basic.show_leds("""
         . . . . .
-        . . . . .
-        . # # # .
-        . # # # .
+        . # # . .
+        . # # . .
+        . # # . .
         . . . . .
         """)
     basic.pause(100)
     basic.show_leds("""
         . . . . .
+        # . # . .
+        # . # . .
+        # . # . .
         . . . . .
-        . # # # .
+        """)
+    basic.show_leds("""
         . . . . .
-        . # # # .
+        . # # . .
+        . # # . .
+        . # # . .
+        . . . . .
         """)
     music.play(music.string_playable("C#5 ", 200),
         music.PlaybackMode.UNTIL_DONE)
-    basic.show_leds("""
-        . . . . .
-        . . . . .
-        . # # # .
-        . # # # .
-        . . . . .
-        """)
-    pass
-def worm():
-    basic.show_leds("""
-            . . . . .
-            . . . . .
-            . # # # # 
-            # # # # .
-            . . . . .
-            """) 
-    basic.pause(10)
-    basic.show_leds("""
-                . . . . .
-                . . . . .
-                # # # # .
-                . # # # #
-                . . . . .
-                """)
-    pass
-        
+
+def on_button_pressed_b():
+    worm()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+basic.show_leds("""
+    . . . . .
+    . # # . .
+    . # # . .
+    . # # . .
+    . . . . .
+    """)
--- main.ts
+++ main.ts
@@ -1,5 +1,41 @@
-function on_button_pressed_b() {
-    worm()
+function worm() {
+    basic.showLeds(`
+        . # . . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . . # . .
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . . # . .
+        `)
+    basic.pause(10)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # . .
+        . # # . .
+        . # # . .
+        . . . . .
+        `)
 }
 
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
@@ -8,46 +44,36 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 function squalk() {
     basic.showLeds(`
         . . . . .
-        . . . . .
-        . # # # .
-        . # # # .
+        . # # . .
+        . # # . .
+        . # # . .
         . . . . .
         `)
     basic.pause(100)
     basic.showLeds(`
         . . . . .
+        # . # . .
+        # . # . .
+        # . # . .
         . . . . .
-        . # # # .
+        `)
+    basic.showLeds(`
         . . . . .
-        . # # # .
+        . # # . .
+        . # # . .
+        . # # . .
+        . . . . .
         `)
     music.play(music.stringPlayable("C#5 ", 200), music.PlaybackMode.UntilDone)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . # # # .
-        . # # # .
-        . . . . .
-        `)
-    
 }
 
-function worm() {
-    basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # # # # 
-            # # # # .
-            . . . . .
-            `)
-    basic.pause(10)
-    basic.showLeds(`
-                . . . . .
-                . . . . .
-                # # # # .
-                . # # # #
-                . . . . .
-                `)
-    
-}
-
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    worm()
+})
+basic.showLeds(`
+    . . . . .
+    . # # . .
+    . # # . .
+    . # # . .
+    . . . . .
+    `)
